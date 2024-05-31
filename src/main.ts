c3efa494d1407d89d355bb2cc35c30a603f4ebdb
madeline <madgrismad@gmail.com>
2024-05-30 23:49:24 -0700

    chore: Add .netlify folder to .gitignore and update npm dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { defineComponent } from 'vue'
+const AppComponent = defineComponent({
+  components: {
+    App
+  }
+})
 
-createApp(App).mount('#app')
+createApp(AppComponent).mount('#app')
