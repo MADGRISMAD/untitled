4c35b1c00ee03d2e6a053eeadb1aedc466fbd071
madeline <madgrismad@gmail.com>
2024-05-30 22:08:56 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-bold mb-4">Control de servidor de Minecraft</h1>
+      <div>
+        <button @click="startServer" :disabled="serverStatus === 'running'" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Iniciar servidor
+        </button>
+        <button @click="stopServer" :disabled="serverStatus === 'stopped'" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Detener servidor
+        </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        serverStatus: 'stopped', 
+      };
+    },
+    methods: {
+      startServer() {
+        
+        this.serverStatus = 'running'; 
+      },
+      stopServer() {
+        
+        this.serverStatus = 'stopped'; 
+      },
+    },
+  };
+  </script>
